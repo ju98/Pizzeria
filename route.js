341ce9a18ffd39acf26d37bc187cea997026af09
Ju98 <juliette.1024@gmail.com>
2019-11-15 14:11:19 +0100

    Ajout d'une route

--- route.js
+++ route.js
@@ -2,8 +2,15 @@ var controller=require('./controller.js');
 
 
 const route=require('express').Router();
-	route.get('/menu', function(req, res) {
+
+	route.get('/polypizz', function(req, res) {
+		res.setHeader('Content-Type', 'text/plain');
+		res.send("Bienvenue dans notre pizzeria Poly'pizz ! ");
+	});
+
+	route.get('/polypizz/menu', function(req, res) {
 		res.setHeader('Content-Type', 'text/plain');
 		res.send("Voici le menu de la pizzeria : "+ controller.afficherDailyMenu());
 	});
+
 module.exports=route;
