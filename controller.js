607aaa1eaf0b53e4737f8d4b78b0bde9a3340948
Ju98 <juliette.1024@gmail.com>
2019-12-11 11:06:57 +0100

    Ajout des routes READ et DELETE avec requetes associées

--- controller.js
+++ controller.js
@@ -7,7 +7,7 @@ module.exports.afficherDailyMenu = function() {
 };
 
 module.exports.apiCreateForm = function() {
-	return '<html><body>CREATE<br /><form method="post" action="create"><input type="number" name="key" /><input type="text" name="name" /><input type="text" name="ingredients" /><input type="text" name="category" /><input type="text" name="price" /><input type="submit" /></form></body></html>';
+	return '<html><body>CREATE<br/><form method="post" action="create"><div><label for="key">Key :</label><input type="number" name="key" /></div><div><label for="name">Name :</label><input type="text" name="name" /></div><div><label for="ingredients">Ingredients :</label><input type="text" name="ingredients" /></div><div><label for="category">Category :</label><input type="text" name="category" /></div><div><label for="price">Price :</label><input type="text" name="price" /></div><input type="submit" /></form></body></html>';
 }
 
 module.exports.apiCreateSave = function(key, name, ingredients, category, price) {
@@ -15,10 +15,34 @@ module.exports.apiCreateSave = function(key, name, ingredients, category, price)
 	answer.save();
 }
 
-var apiRead='';
-module.exports.apiRead = function(key) {
+module.exports.apiRead = function(res, key) {
 	model.Answer.findOne({key: key},function(err,answer){
-		apiRead=answer.name;
+		if(err){console.log(err);}
+		res.send('READ : Value equals '+answer.name+' '+answer.ingredients+' '+answer.category+' '+answer.price+'.');
 	});
-	return apiRead;
+}
+
+module.exports.apiDeleteForm = function(res) {
+	model.Answer.find({}, function(err, answer) {
+		var result = '<html><body>DELETE<br/><form method="post" action="delete"><div><label for="key">Key :</label><select name="key">';
+		var keys = [];
+		for (i in answer) {
+			keys.push(answer[i].key);
+		}
+		for (i in keys) {
+			result += '<option value="'+keys[i]+'">'+keys[i]+'</option>';
+		}
+		res.send(result+'</select><input type="submit" /></form></body></html>');
+	});
+}
+
+module.exports.apiDelete = function(res, key) {
+	/*model.Answer.find({key: key}, function(err, answer) {
+			console.log(answer);
+	});*/
+	model.Answer.deleteOne({key: key}, function(err, answer) {
+		if(err){console.log(err);}
+		else{res.send('DELETE : Variable '+key+' reduces to 0.');}
+	});
+
 }
--- route.js
+++ route.js
@@ -1,7 +1,8 @@
 var controller=require('./controller.js');
 
 const route=require('express').Router();
-
+	
+	//CREATE
 	route.get('/polypizz/api/create',(req,res)=>{
 		res.send(controller.apiCreateForm());
 	});
@@ -11,18 +12,29 @@ const route=require('express').Router();
 		res.send('CREATE : Variable initialized at '+req.body.name+'.');
 	});
 	
-	route.get('/polypizz/api/read/:id',(req,res)=>{
-		res.send('READ : Value equals '+controller.apiRead(req.params.id)+'.');
+	//READ
+	route.get('/polypizz/api/read/:key',(req,res)=>{
+		controller.apiRead(res, req.params.key);
+		//res.send('READ : Value equals '+controller.apiRead(req.params.key)+'.');
 	});
 	
+	//UPDATE
 	route.get('/polypizz/api/update',(req,res)=>{
 		res.send('UPDATE : Variable incremented.');
 	});
 	
+	//DELETE
 	route.get('/polypizz/api/delete',(req,res)=>{
-		res.send('DELETE : Variable reduces to 0.');
+		//res.send('DELETE : Variable reduces to 0.');
+		controller.apiDeleteForm(res);
+	});
+
+	route.post('/polypizz/api/delete',(req,res)=>{
+		controller.apiDelete(res, req.body.key);
+		//res.send('DELETE : Variable '+req.body.key+' reduces to 0.');
 	});
 	
+	//
 	route.get('/polypizz',(req,res)=>{
 		res.send("Express response. Bienvenue dans notre pizzeria Poly'pizz !");
 	});
